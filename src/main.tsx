dd76a4a305a4f1459f2a318882ca206265974c6f
Figar <figar.januari@gmail.com>
2024-08-03 17:21:14 +0700

    add prod irl

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import './index.css'
 import TodosContextProvider from './contexts/TodosContextProvider.tsx'
 import { KindeProvider } from '@kinde-oss/kinde-auth-react'
 
+const url = process.env.NODE_ENV === "production"
+              ? 'https://react-todo-list-one-blush.vercel.app'
+              : 'http://localhost:3000'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <KindeProvider
       clientId="98268c3729a54cab96fc7ffead9c3f2b"
       domain="https://gartest.kinde.com"
-      redirectUri="http://localhost:3000"
-      logoutUri="http://localhost:3000"
+      redirectUri={url}
+      logoutUri={url}
     >
       <TodosContextProvider>
         <App />
